f2d7e8b447f5ec587683472fc39403753e1f7976
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2022-06-05 20:28:32 -0700

    Update mf

--- static/js/camera.js
+++ static/js/camera.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
     var localMediaStream = null;
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
     
+    /*
     function sendSnapshot() {
       if (!localMediaStream) {
         return;
@@ -22,6 +23,7 @@ $(document).ready(function(){
       }
       );
     }
+    */
     
     socket.on('connect', function() {
       console.log('Connected!');
